9c55a63db0b60c6fe2d95e3eef38fd889a653345
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-16 15:13:53 +0100

    suprememods

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { SearchIcon } from "@heroicons/react/outline"
 export default function Header() {
     return(
         <div className="w-full shadow-black shadow flex justify-center items-center py-5">
-            <div className="max-w-6xl w-full flex justify-between items-center">
+            <div className="max-w-[750px] lg:max-w-4xl xl:max-w-6xl w-full flex justify-between items-center">
                 <div>
                     <Image src={"/logo.png"} width={200} height={50} />
                 </div>
