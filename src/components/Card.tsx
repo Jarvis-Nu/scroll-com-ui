889eb2d2789a2616afb35b53850ff7aac9987d85
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-16 16:23:30 +0100

    suprememods

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,15 @@
 import Image from "next/legacy/image";
+import Link from "next/link";
 
 export default function Card() {
     return(
-        <div className="max-w-[300px] h-full p-2.5 flex flex-col items-center">
-            <div className="bg-black w-[275px] h-[225px] relative">
-                <Image src={"/image.jpg"} alt="image" layout="fill" />
+        <Link href={"/"}>
+            <div className="max-w-[300px] h-full p-2.5 flex flex-col items-center space-y-1 bg-white hover:scale-105">
+                <div className="bg-black w-[275px] xl:w-[250px] h-[225px] relative">
+                    <Image src={"/image.jpg"} alt="image" layout="fill" />
+                </div>
+                <h3 className="text-xl w-[275px] xl:w-[250px]">Fngeen Men's Luxury Diamond Waterproof Quartz Watch - Gold</h3>
             </div>
-            <h3 className="text-2xl w-[275px]">Fngeen Men's Luxury Diamond Waterproof Quartz Watch - Gold</h3>
-        </div>
+        </Link>
     )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 export default function Main() {
     return(
         <div className="max-w-[750px] lg:max-w-4xl xl:max-w-6xl w-full flex justify-between items-center bg-white">
-            <div className="w-full grid xl:grid-cols-4 gap-x-2 gap-y-2 place-items-center">
+            <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-2 gap-y-2 place-items-center">
                 <div>
                     <Card />
                 </div>
