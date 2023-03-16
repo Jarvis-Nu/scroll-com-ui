2cbdbd07add094001e52beca2353d0cd925ddaaf
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-16 16:11:33 +0100

    suprememods

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,10 +2,11 @@ import Image from "next/legacy/image";
 
 export default function Card() {
     return(
-        <div className="max-w-[300px] h-full p-2.5">
-            <div className="bg-black w-[250px] h-[225px] relative">
+        <div className="max-w-[300px] h-full p-2.5 flex flex-col items-center">
+            <div className="bg-black w-[275px] h-[225px] relative">
                 <Image src={"/image.jpg"} alt="image" layout="fill" />
             </div>
+            <h3 className="text-2xl w-[275px]">Fngeen Men's Luxury Diamond Waterproof Quartz Watch - Gold</h3>
         </div>
     )
 }
