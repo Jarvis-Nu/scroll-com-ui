86e8b082e41a0d226e09c8310d831c586bbe379c
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-16 15:11:51 +0100

    suprememods

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline"
 
 export default function Header() {
     return(
@@ -7,7 +8,16 @@ export default function Header() {
                 <div>
                     <Image src={"/logo.png"} width={200} height={50} />
                 </div>
-                <div></div>
+                <div className="flex items-center">
+                    <div>
+                        <input type="text" className="h-14 w-72 border-2 border-black outline-none p-2.5" placeholder="Search for item" />
+                    </div>
+                    <div className="border-2 border-black h-14 border-l-0 flex flex-col items-center justify-center px-2.5">
+                        <button>
+                            <SearchIcon className="w-10 h-10" />
+                        </button>
+                    </div>
+                </div>
                 <div>
                     <button className="p-4 bg-black text-white">
                         Join Seller{"'"}s Dao
